2be80cdbe2baca9f06d6ff7319e1400f0f76f0de
Denis Gukov <denis.guk@konomic.com>
2024-01-10 12:47:23 +0300

    deploy: fixed deploy issues, fixed login and register settings issues

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,40 +5,48 @@ const jWebToken = require('jsonwebtoken');
 
 class UserController {
     async registration(req, res, next) {
-        const {email, password} = req.body;
-        if (!email || !password) {
-            return next(ApiError.badRequest('Bad password or email'));
+        try {
+            const {email, password} = req.body;
+            if (!email || !password) {
+                return next(ApiError.badRequest('Bad password or email'));
+            }
+            const existedUser = await User.findOne({ where: {email}});
+            if (existedUser) {
+                return next(ApiError.badRequest('Пользователь с такими данными уже существует'));
+            }
+            const hashPassword = await bcrypt.hash(password, 5);
+            const user = await User.create({email, password: hashPassword, settings: JSON.stringify({}), spectated_users: []});
+            const token = jWebToken.sign(
+                {id: user.id, email: user.email, settings: user.settings, spectated_users: user.spectated_users},
+                process.env.SECRET_KEY,
+                {expiresIn: '24h'}
+            );
+            return res.json({status: 200, error: false, token});
+        } catch {
+            return res.status(500).json({message: 'Unhandled error', error: true, status: 500})
         }
-        const existedUser = await User.findOne({ where: {email}});
-        if (existedUser) {
-            return next(ApiError.badRequest('Пользователь с такими данными уже существует'));
-        }
-        const hashPassword = await bcrypt.hash(password, 5);
-        const user = await User.create({email, password: hashPassword, settings: {}, spectated_users: []});
-        const token = jWebToken.sign(
-            {id: user.id, email: user.email, settings: JSON.stringify({}), spectated_users: []},
-            process.env.SECRET_KEY,
-            {expiresIn: '24h'}
-        );
-        return res.json({status: 200, error: false, token});
     }
     async login(req, res, next) {
-        const { email, password } = req.body;
-        const user = await User.findOne({where: {email}});
-        if (!user) {
-            return next(ApiError.badRequest('Пользователь не найден'));
+        try {
+            const { email, password } = req.body;
+            const user = await User.findOne({where: {email}});
+            if (!user) {
+                return next(ApiError.badRequest('Пользователь не найден'));
+            }
+            let comparePasword = bcrypt.compareSync(password, user.password);
+            if (!comparePasword) {
+                return next(ApiError.badRequest('Указан неверный пароль'));
+            }
+            const responseUser = {id: user.id, email: user.email, settings: JSON.parse(user.settings), spectated_users: user.spectated_users};
+            const token = jWebToken.sign(
+                responseUser,
+                process.env.SECRET_KEY,
+                {expiresIn: '24h'}
+            );
+            return res.json({status: 200, error: false, token, user: responseUser});
+        } catch {
+            return res.status(500).json({message: 'Unhandled error', error: true, status: 500})
         }
-        let comparePasword = bcrypt.compareSync(password, user.password);
-        if (!comparePasword) {
-            return next(ApiError.badRequest('Указан неверный пароль'));
-        }
-        const responseUser = {id: user.id, email: user.email, settings: JSON.parse(user.settings), spectated_users: user.spectated_users};
-        const token = jWebToken.sign(
-            responseUser,
-            process.env.SECRET_KEY,
-            {expiresIn: '24h'}
-        );
-        return res.json({status: 200, error: false, token, user: responseUser});
     }
     async check(req, res, next) {
         try {
